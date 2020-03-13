0a5d4ce088f9a92b759c15879681f760970c34e7
yqrashawn <namy.19@gmail.com>
2020-03-13 18:25:34 +0800

    Add: finish gen sidebar config

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -1,95 +1,102 @@
+const { resolve } = require("path");
+
 module.exports = {
-  title: 'My Site',
-  tagline: 'The tagline of my site',
-  url: 'https://your-docusaurus-test-site.com',
-  baseUrl: '/',
-  favicon: 'img/favicon.ico',
-  organizationName: 'facebook', // Usually your GitHub org/user name.
-  projectName: 'docusaurus', // Usually your repo name.
+  title: "My Site",
+  tagline: "The tagline of my site",
+  url: "https://your-docusaurus-test-site.com",
+  baseUrl: "/",
+  favicon: "img/favicon.ico",
+  organizationName: "facebook", // Usually your GitHub org/user name.
+  projectName: "docusaurus", // Usually your repo name.
   themeConfig: {
+    sidebarCollapsible: false,
     navbar: {
-      title: 'Conflux Developer',
+      title: "Conflux Developer",
       logo: {
-        alt: 'Conflux Logo',
-        src: 'img/logo.svg',
+        alt: "Conflux Logo",
+        src: "img/logo.svg"
       },
       links: [
-        {to: 'docs/doc1', label: 'Docs', position: 'left'},
-        {to: 'blog', label: 'Blog', position: 'left'},
         {
-          href: 'https://github.com/facebook/docusaurus',
-          label: 'GitHub',
-          position: 'right',
+          to: "docs/introduction/en/what_is_conflux",
+          label: "Docs",
+          position: "left"
         },
-      ],
+        { to: "blog", label: "Blog", position: "left" },
+        {
+          href: "https://github.com/facebook/docusaurus",
+          label: "GitHub",
+          position: "right"
+        }
+      ]
     },
     footer: {
-      style: 'dark',
+      style: "dark",
       links: [
         {
-          title: 'Docs',
+          title: "Docs",
           items: [
             {
-              label: 'Style Guide',
-              to: 'docs/doc1',
+              label: "Style Guide",
+              to: "docs/doc1"
             },
             {
-              label: 'Second Doc',
-              to: 'docs/doc2',
-            },
-          ],
+              label: "Second Doc",
+              to: "docs/doc2"
+            }
+          ]
         },
         {
-          title: 'Community',
+          title: "Community",
           items: [
             {
-              label: 'Stack Overflow',
-              href: 'https://stackoverflow.com/questions/tagged/docusaurus',
+              label: "Stack Overflow",
+              href: "https://stackoverflow.com/questions/tagged/docusaurus"
             },
             {
-              label: 'Discord',
-              href: 'https://discordapp.com/invite/docusaurus',
-            },
-          ],
+              label: "Discord",
+              href: "https://discordapp.com/invite/docusaurus"
+            }
+          ]
         },
         {
-          title: 'Social',
+          title: "Social",
           items: [
             {
-              label: 'Blog',
-              to: 'blog',
+              label: "Blog",
+              to: "blog"
             },
             {
-              label: 'GitHub',
-              href: 'https://github.com/Conflux-Chain',
+              label: "GitHub",
+              href: "https://github.com/Conflux-Chain"
             },
             {
-              label: 'Twitter',
-              href: 'https://twitter.com/confluxchain',
-            },
-          ],
-        },
+              label: "Twitter",
+              href: "https://twitter.com/confluxchain"
+            }
+          ]
+        }
       ],
-      copyright: `Copyright © ${new Date().getFullYear()} Conflux Chain, Inc. Built with Docusaurus.`,
-    },
+      copyright: `Copyright © ${new Date().getFullYear()} Conflux Chain, Inc. Built with Docusaurus.`
+    }
   },
   presets: [
     [
-      '@docusaurus/preset-classic',
+      "@docusaurus/preset-classic",
       {
         docs: {
-          path: 'docs',
-          routeBasePath: 'docs',
-          sidebarPath: require.resolve('./sidebars.en.js'),
+          path: "docs",
+          routeBasePath: "docs",
+          sidebarPath: resolve(__dirname, "sidebars.en.js" ),
           editUrl:
-            'https://github.com/facebook/docusaurus/edit/master/website/',
+            "https://github.com/facebook/docusaurus/edit/master/website/",
           showLastUpdateTime: true,
-          showLastUpdateAuthor: true,
+          showLastUpdateAuthor: true
         },
         theme: {
-          customCss: require.resolve('./src/css/custom.css'),
-        },
+          customCss: require.resolve("./src/css/custom.css")
+        }
       }
-    ],
-  ],
+    ]
+  ]
 };
