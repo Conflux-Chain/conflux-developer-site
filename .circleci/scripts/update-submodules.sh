0ec3a330afff11cf02a75bc28a4265b1ac376833
yqrashawn <namy.19@gmail.com>
2020-03-17 15:17:37 +0800

    Fix: error when no markdown changed

--- .circleci/scripts/update-submodules.sh
+++ .circleci/scripts/update-submodules.sh
@@ -9,19 +9,16 @@ git submodule update --init
 changed=$(git submodule foreach git diff --no-color \
               origin/master \
             | grep -E '^(---|\+\+\+)\ (a|b).*\.md[x]?$'\
-            || git diff --no-color HEAD~ | grep -E '^(---|\+\+\+)\ (a|b).*\.md[x]?$')
+            || git diff --no-color HEAD~ | grep -E '.*' || true)
 
-# exit code
-retVal=$?
+# git config --global user.email $GH_EMAIL
+# git config --global user.name $GH_NAME
+# git submodule foreach git reset --hard origin/master
+# git add --update
+# git commit -m "Automated update submodules ${CIRCLE_SHA1} [ci skip]" --allow-empty 1> /dev/null
+# git push origin master
 
-git config --global user.email $GH_EMAIL
-git config --global user.name $GH_NAME
-git submodule foreach git reset --hard origin/master
-git add --update
-git commit -m "Automated update submodules ${CIRCLE_SHA1} [ci skip]" --allow-empty 1> /dev/null
-git push origin master
-
-if [ $retVal -eq 0 ]; then
+if [ ${#changed} -ge 1 ]; then
   # found changed fiile don't skip ci
   echo false > .skip-ci
 else
