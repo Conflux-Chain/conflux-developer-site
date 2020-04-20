6f71f6a4ff22ea8a9c4db266b54b66701cc5c64d
yqrashawn <namy.19@gmail.com>
2020-04-20 15:04:45 +0800

    Fix: parser
    Automated update submodules 3d7bf8f44ba86c1972c74ca34d7b51b7308c9f56 [ci skip]
    
    Automated update submodules 40923c50aef4845328d4322914f53abeeae31288 [ci skip]
    
    Automated update submodules 29dd23b3265665b2b3c62f5f3d1fcef70f3cafda [ci skip]
    
    Automated update submodules 0560f05109fa36809f40613aedf1d31c61c0de51 [ci skip]
    
    Automated update submodules f457a04860e4ae5073c510978764763b76ed4568 [ci skip]
    
    Automated update submodules f11eb5dbc56a7c3b2343e9482f78ee93878d63ee [ci skip]
    
    Automated update submodules 0cad9a616ebc96d2e7d3266b5fe033ec56ea4630 [ci skip]
    
    Automated update submodules 5423bca0b0ea473d27c34583efd6ee279c51f4b2 [ci skip]
    
    Automated update submodules 4077a0060248a6ee82752ca1409d1c04616015a2 [ci skip]
    
    Automated update submodules 1c1ffc5b43c7f6ca4bc766d105a8e264210379e8 [ci skip]
    
    Automated update submodules 4555f2cfffbe77bffe895eb922318e8cb4b7796e [ci skip]
    
    Automated update submodules 3efcf6e61c38f6bc439dd48cd4c9b7307ea6c236 [ci skip]
    
    Automated update submodules 936f1a28083b6ab4587a6f8917e924be75490974 [ci skip]
    
    Automated update submodules e60979e4861f079e5234b41e4efb1dbe1099f422 [ci skip]
    
    Automated update submodules fd43892f20a4f3a11fcc7a4315b3579e768b53cf [ci skip]
    
    Automated update submodules 8559bc86853b7557302f8f266422ca654033fe57 [ci skip]
    
    Automated update submodules 6b9c3930212c1a895b04d3a9ba3cb10da137e160 [ci skip]
    
    Automated update submodules 9a63350369fb8dffb84913bee7335c7110af1e8e [ci skip]
    
    Automated update submodules 9927e32075ea9f2038369d5731adc2653d04f674 [ci skip]
    
    Automated update submodules dd6f965d7c1bfa7eac73ee3a50e8b3fa54f84406 [ci skip]

--- development/rehype-strip-styles-in-md.js
+++ development/rehype-strip-styles-in-md.js
@@ -1,13 +1,19 @@
+const HtmlToReactParser = require("html-to-react").Parser;
+const reactElementToJSXString = require("react-element-to-jsx-string");
+
 module.exports = function () {
   return transformer;
   function transformer(tree) {
     const replaced = tree;
     replaced.children = tree.children.reduce((acc, node) => {
-      // NOTE: this only strips the first level of this kinds of nodes
-      if (node.value && node.value.includes && node.value.includes("style=")) {
-      } else {
-        acc.push(node);
+      if (node.children) node = transformer(node);
+      if (node.type === "jsx" && node.value && node.value.includes('style=')) {
+        const htmlToReactParser = new HtmlToReactParser();
+        const reactElement = htmlToReactParser.parse(node.value);
+        if (!Array.isArray(reactElement))
+          node.value = reactElementToJSXString.default(reactElement).replace(/\n/g, '');
       }
+      acc.push(node);
       return acc;
     }, []);
     return replaced;
