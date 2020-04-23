b2fba343df2929185ae512c83c32fe22a2374fb4
yqrashawn <namy.19@gmail.com>
2020-04-23 18:30:02 +0800

    Add: home page svg
    Automated update submodules 8845fe8f78416ccf8ffd5b1ca8ad4fb252bc4e62 [ci skip]
    
    Automated update submodules b5416407fe8c8b3f297211259188c54049dc7d30 [ci skip]
    
    Automated update submodules 644fecb837661f4667fa33ce224efa148d28a0af [ci skip]

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,77 +1,94 @@
-import React from 'react';
-import classnames from 'classnames';
-import Layout from '@theme/Layout';
-import Link from '@docusaurus/Link';
-import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
-import useBaseUrl from '@docusaurus/useBaseUrl';
-import styles from './styles.module.css';
+import React from "react";
+import classnames from "classnames";
+import Layout from "@theme/Layout";
+import useThemeContext from '@theme/hooks/useThemeContext';
+import Link from "@docusaurus/Link";
+import useDocusaurusContext from "@docusaurus/useDocusaurusContext";
+import useBaseUrl from "@docusaurus/useBaseUrl";
+import styles from "./styles.module.css";
 
 const features = [
   {
-    title: <>Easy to Use</>,
-    imageUrl: 'img/undraw_docusaurus_mountain.svg',
+    title: <>Scalability</>,
+    imageUrl: {
+      dark: "img/scalability-dark.svg",
+      light: "img/scalability-light.svg",
+    },
     description: (
       <>
-        Docusaurus was designed from the ground up to be easily installed and
-        used to get your website up and running quickly.
+        By adopting an scalability consensus mechanism and safely retaining fork
+        blocks, Conflux realized a high throughput rate of 3000-6000 TPS in our
+        real-network testing.
       </>
     ),
   },
   {
-    title: <>Focus on What Matters</>,
-    imageUrl: 'img/undraw_docusaurus_tree.svg',
+    title: <>Extensibility</>,
+    imageUrl: {
+      dark: "img/extensive-dark.svg",
+      light: "img/extensive-light.svg",
+    },
     description: (
       <>
-        Docusaurus lets you focus on your docs, and we&apos;ll do the chores. Go
-        ahead and move your docs into the <code>docs</code> directory.
+        Blockchain oracle combined with cryptographic tools to realize powerful
+        features such as external data feeding, confidential data control, and
+        beyond.
       </>
     ),
   },
   {
-    title: <>Powered by React</>,
-    imageUrl: 'img/undraw_docusaurus_react.svg',
+    title: <>Security</>,
+    imageUrl: {
+      dark: "img/security-dark.svg",
+      light: "img/security-light.svg",
+    },
     description: (
       <>
-        Extend or customize your website layout by reusing React. Docusaurus can
-        be extended while reusing the same header and footer.
+        Ensured by 2 Dimensional protection - Anti double-spend attack
+        protection (consensus-level) and contract code proofreading
+        (application-level).
       </>
     ),
   },
 ];
 
-function Feature({imageUrl, title, description}) {
-  const imgUrl = useBaseUrl(imageUrl);
+function Feature({ imageUrl, title, description }) {
+  const { isDarkTheme } = useThemeContext();
+  const theme = isDarkTheme ? "dark" : "light";
+  const imgUrl = useBaseUrl(imageUrl[theme]);
   return (
-    <div className={classnames('col col--4', styles.feature)}>
+    <div className={classnames("col col--4", styles.feature)}>
       {imgUrl && (
         <div className="text--center">
           <img className={styles.featureImage} src={imgUrl} alt={title} />
         </div>
       )}
-      <h3>{title}</h3>
-      <p>{description}</p>
+      <h3 className="text--center">{title}</h3>
+      <p className="text--center">{description}</p>
     </div>
   );
 }
 
 function Home() {
   const context = useDocusaurusContext();
-  const {siteConfig = {}} = context;
+  const { siteConfig = {} } = context;
   return (
     <Layout
       title={`Hello from ${siteConfig.title}`}
-      description="Description will go into a meta tag in <head />">
-      <header className={classnames('hero hero--primary', styles.heroBanner)}>
+      description="Description will go into a meta tag in <head />"
+    >
+      <header className={classnames("hero hero--primary", styles.heroBanner)}>
         <div className="container">
           <h1 className="hero__title">{siteConfig.title}</h1>
           <p className="hero__subtitle">{siteConfig.tagline}</p>
           <div className={styles.buttons}>
             <Link
               className={classnames(
-                'button button--outline button--secondary button--lg',
-                styles.getStarted,
+                "button button--outline button--secondary button--lg",
+                styles.getStarted
               )}
-              to={useBaseUrl('/docs/introduction/en/conflux_overview')}>
+              to={useBaseUrl("/docs/introduction/en/conflux_overview")}
+            >
               Get Started
             </Link>
           </div>
@@ -83,7 +100,11 @@ function Home() {
             <div className="container">
               <div className="row">
                 {features.map((props, idx) => (
-                  <Feature key={idx} {...props} />
+                  <Feature
+                    key={idx}
+                    {...props}
+                    imageUrl={props.imageUrl}
+                  />
                 ))}
               </div>
             </div>
