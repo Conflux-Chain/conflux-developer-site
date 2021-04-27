900175eafb286c5b8becd5f7db3dfae2fec5a2c1
PanaW <wangpan@conflux-chain.org>
2021-04-27 10:55:30 +0800

    inital version

--- README.md
+++ README.md
@@ -1,47 +1,33 @@
-# Conflux Developer Website
+# Website
 
-This website is built using [Docusaurus 2](https://v2.docusaurus.io/), a modern
-static website generator. It supports retrieving docs from any repository and
-deploys to developer.conflux-chain.org.
+This website is built using [Docusaurus 2](https://docusaurus.io/), a modern static website generator.
 
-To submit a repository, please read the doc [here](./docs/submit-a-repo.md).
+## Installation
 
-### Installation
-
-```shell
-$ git clone --recurse-submodules https://github.com/Conflux-Chain/conflux-developer-site
-$ cd conflux-developer-site
-$ yarn
+```console
+yarn install
 ```
 
-**Note: the `yarn REINIT` command will run a shell script that clears any
-uncommitted changes under this directory including all submodules in `docs`
-folder. It will set everything to the same state as your `origin/master`.** 
+## Local Development
 
-### Local Development
-
-```
-$ yarn start
+```console
+yarn start
 ```
 
-This command starts a local development server and open up a browser window.
-Most changes are reflected live without having to restart the server.
+This command starts a local development server and opens up a browser window. Most changes are reflected live without having to restart the server.
 
-### Build
+## Build
 
+```console
+yarn build
 ```
-$ yarn build
+
+This command generates static content into the `build` directory and can be served using any static contents hosting service.
+
+## Deployment
+
+```console
+GIT_USER=<Your GitHub username> USE_SSH=true yarn deploy
 ```
 
-This command generates static content into the `build` directory and can be
-served using any static contents hosting service.
-
-### Contributing
-
-Want to contribute to Conflux developer site? Awesome! You can find information about
-contributing to this project in
-[issues](https://github.com/Conflux-Chain/conflux-developer-site/issues)
-
-If you don't know how to make your first contributing, please reference here:
-[First Contributions](https://github.com/firstcontributions/first-contributions) 
-
+If you are using GitHub pages for hosting, this command is a convenient way to build the website and push to the `gh-pages` branch.
---
+++ docusaurus.config.js
@@ -0,0 +1,118 @@
+/** @type {import('@docusaurus/types').DocusaurusConfig} */
+module.exports = {
+  title: 'Conflux',
+  tagline: 'Where Valuable Bits are Exchanged and Validated',
+  url: 'https://developer.conflux-chain.org',
+  baseUrl: '/',
+  onBrokenLinks: 'throw',
+  onBrokenMarkdownLinks: 'warn',
+  favicon: 'img/favicon.ico',
+  organizationName: 'Conflux-Chain', // Usually your GitHub org/user name.
+  projectName: 'conflux-developer-site', // Usually your repo name.
+  themeConfig: {
+    navbar: {
+      title: 'Conflux Developer',
+      logo: {
+        alt: 'Conflux Logo',
+        src: 'img/logo.svg',
+      },
+      items: [
+        {
+          type: 'doc',
+          docId: 'guides/en/how_to_use_cfx_faucet',
+          position: 'left',
+          label: 'Guides',
+        },
+        {
+          type: 'doc',
+          docId: 'introduction/en/conflux_overview',
+          position: 'left',
+          label: 'Docs',
+        },
+        {
+          type: 'doc',
+          docId: 'conflux-doc/docs/json_rpc',
+          position: 'left',
+          label: 'APIs',
+        },
+        // {to: '/blog', label: 'Blog', position: 'left'},
+        {
+          href: 'https://github.com/conflux-chain/conflux-rust',
+          label: 'GitHub',
+          position: 'right',
+        },
+      ],
+    },
+    footer: {
+      style: "dark",
+      links: [
+        {
+          title: 'Docs',
+          items: [
+            {
+              label: "Overview",
+              to: "docs/introduction/en/conflux_overview"
+            },
+            {
+              label: "Portal",
+              to: "https://conflux-portal-docs.conflux-chain.org/docs/portal/introduction/",
+            },
+          ],
+        },
+        {
+          title: 'Community',
+          items: [
+            {
+              label: "Bounty",
+              href: "https://bounty.conflux-chain.org",
+            },
+            {
+              label: "Forum",
+              href: "https://forum.conflux.fun/"
+            }
+          ],
+        },
+        {
+          title: 'Social',
+          items: [
+            // {
+            //   label: "Blog",
+            //   to: "blog"
+            // },
+            {
+              label: "GitHub",
+              href: "https://github.com/Conflux-Chain",
+            },
+            {
+              label: "Twitter",
+              href: "https://twitter.com/Conflux_Network",
+            },
+          ],
+        },
+      ],
+      copyright: `© ${new Date().getFullYear()} Conflux. All Rights Reserved.`,
+    },
+  },
+  presets: [
+    [
+      '@docusaurus/preset-classic',
+      {
+        docs: {
+          sidebarPath: require.resolve('./sidebars.js'),
+          // Please change this to your repo.
+          editUrl:
+            'https://github.com/facebook/docusaurus/edit/master/website/',
+        },
+        blog: {
+          showReadingTime: true,
+          // Please change this to your repo.
+          editUrl:
+            'https://github.com/facebook/docusaurus/edit/master/website/blog/',
+        },
+        theme: {
+          customCss: require.resolve('./src/css/custom.css'),
+        },
+      },
+    ],
+  ],
+};
